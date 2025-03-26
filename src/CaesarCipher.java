fd00fdcba96250cfd3f66e82b1e7d377d6a2b36a
alirashidAR <alirashid.38@yahoo.com>
2025-03-27 03:43:34 +0530

    inital commit

---
+++ src/CaesarCipher.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class CaesarCipher {
+
+    public static String caesarCipher(String text, int shift){
+        StringBuilder result = new StringBuilder();
+        for(char ch: text.toCharArray()){
+            if(Character.isLetter(ch)){
+                char base = Character.isUpperCase(ch)?'A':'a';
+                result.append((char)((ch-base+shift+26)%26+base));
+            }else{
+                result.append(ch);
+            }
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args){
+        Scanner sc  = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String text = sc.nextLine();
+        System.out.print("Enter a shift: ");
+        int shift = sc.nextInt();
+        String encryptedText = caesarCipher(text, shift);
+        System.out.println("Caesar cipher text: " + encryptedText);
+        System.out.println("Decypted text: " + caesarCipher(encryptedText, -shift));
+    }
+}
